a475c0b288beebd64982de9442651ffee774b0f8
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2025-01-27 17:54:38 -0800

    t

--- rateagame/src/routes/reviews/submit.ts
+++ rateagame/src/routes/reviews/submit.ts
@@ -10,11 +10,11 @@ const prisma = new PrismaClient();
 export const submit = async (c: Context) => {
   const requestData = await c.req.json().catch(() => null); // catch in case no JSON is sent
 
-  let gameId: String = requestData.gameId;
+  let gameId: string = requestData.gameId;
   let date = new Date(requestData.date * 1000);
   let text = requestData.text;
   let recommends = requestData.recommends;
-  let userId: String = requestData.userId;
+  let userId: string = requestData.userId;
   let reviewId = requestData.reviewId;
   let gamePass = requestData.gamePass || false; //check if this works properly, it might be a string and not a boolean
   //make sure that when looping out all user generated content u are using roblox's filter system
@@ -23,7 +23,7 @@ export const submit = async (c: Context) => {
     let player: any = await playerCheck(userId);
     let game: any = await gameCheck(gameId, gamePass);
 
-    if (text.length < 2001 && date.length < 2001) {
+    if (text.length < 2001) {
       const newreviewdata = await prisma.reviewData.create({
         data: {
           reviewId,
