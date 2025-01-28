4b9e476a5b4ea91590abb1b465aad804b726a45a
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2025-01-27 17:10:06 -0800

    bruh

--- rateagame/src/routes/reviews/submit.ts
+++ rateagame/src/routes/reviews/submit.ts
@@ -18,7 +18,14 @@ export const submit = async (c: Context) => {
   let reviewId = requestData.reviewId;
   let gamePass = requestData.gamePass; //check if this works properly, it might be a string and not a boolean
   //make sure that when looping out all user generated content u are using roblox's filter system
-  console.log("test");
+  console.log(gameId);
+  console.log(date);
+  console.log(text);
+  console.log(recommends);
+  console.log(userId);
+  console.log(reviewId);
+  console.log(gamePass);
+
   if (gameId & date & text & recommends & userId & gamePass) {
     await playerCheck(userId);
     await gameCheck(gameId, gamePass);
