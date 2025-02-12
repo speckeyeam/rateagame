0336fa365e2800fbf5ed27dbcd654208ec13a6ce
speckeyeam <dicklung212@gmail.com>
2025-02-11 20:54:17 -0800

    update points

--- rateagame/src/routes/player/updatePoints.ts
+++ rateagame/src/routes/player/updatePoints.ts
@@ -1,2 +1,32 @@
-//GO BACK TO THIS, CREATE A SCRIPT ON STUDIO THAT SAVES THE AMOUNT OF POINTS THAT EVERY USER HAS EVERY 10 MINUTES.
-//IT WILL SAVE TEH AMOUNT OF POINTS WHEN THE PLAYERS LEAVE.
+import { Context } from "hono";
+
+import { PrismaClient } from "@prisma/client";
+
+import { playerCheck } from "../helpers/playerCheck";
+import { gameCheck } from "../helpers/gameCheck";
+
+const prisma = new PrismaClient();
+
+export const getPoints = async (c: Context) => {
+  const requestData = await c.req.json().catch(() => null); // catch in case no JSON is sent
+
+  const { userId, token } = requestData;
+
+  if (userId && token) {
+    let player: any = await playerCheck(userId, token);
+    if (player) {
+      const updatedUser = await prisma.user.update({
+        where: { userId },
+        data: {
+          coins: {
+            increment: 500, // Increase coins by 500
+          },
+        },
+      });
+      if (updatedUser) {
+        return c.json({ success: true, points: updatedUser.coins }, 200);
+      }
+    }
+  }
+  return c.json({ success: false }, 500);
+};
