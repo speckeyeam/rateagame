fa034f741cb301202d075561f1a8149df8971016
speckeyeam <dicklung212@gmail.com>
2025-02-12 23:18:39 -0800

    test

--- rateagame/src/routes/awards/giveAward.ts
+++ rateagame/src/routes/awards/giveAward.ts
@@ -12,27 +12,72 @@ const prisma = new PrismaClient();
 export const giveAward = async (c: Context) => {
   const requestData = await c.req.json().catch(() => null); // catch in case no JSON is sent
 
-  const { userId, token, awardId, buying, reviewId, recieverId } =
+  const { userId, token, awardId, buying, reviewId, gamePass, gameId } =
     await requestData;
 
   console.log(requestData);
-  if (userId && token && awardId && reviewId) {
+  if (userId && token && awardId && reviewId && gameId) {
     let player: any = await playerCheck(userId, token);
     let award = await awardCheck(awardId);
     if (player && award) {
-      if (buying) {
-        const saleCheck = await awardIsForSale(award);
-        if (saleCheck) {
-          if (player.coins >= award.price) {
-            const updatedUser = await prisma.user.update({
-              where: { userId: userId.toString() },
-              data: {
-                coins: {
-                  increment: -award.price, // Increase coins by 500
+      const review = await prisma.review.findFirst({
+        where: {
+          reviewId: reviewId.toString,
+          [gamePass ? "gamePassId" : "gameId"]: gameId,
+        },
+      });
+
+      if (review) {
+        const recieverId = review.userId;
+        if (buying && recieverId) {
+          const saleCheck = await awardIsForSale(award);
+          if (saleCheck) {
+            if (player.coins >= award.price) {
+              const updatedUser = await prisma.user.update({
+                where: { userId: userId.toString() },
+                data: {
+                  coins: {
+                    increment: -award.price, // Increase coins by 500
+                  },
                 },
+              });
+              if (updatedUser) {
+                const givenAward = await prisma.award.create({
+                  data: {
+                    givenUserId: userId.toString(),
+                    receivedUserId: recieverId.toString(),
+                    reviewId: reviewId.toString(),
+                    awardId: award.id.toString(),
+                    time: new Date(),
+                  },
+                });
+                if (givenAward) {
+                  return c.json(
+                    {
+                      success: true,
+                      updatedPoints: player.coins - award.price,
+                    },
+                    200
+                  );
+                }
+              }
+            }
+          }
+        } else if (recieverId) {
+          const hasAward = await prisma.awardInventory.findFirst({
+            where: {
+              userId: userId.toString(),
+              awardId: award.id,
+            },
+          });
+          if (hasAward) {
+            const deleteAward = await prisma.awardInventory.delete({
+              where: {
+                userId: userId.toString(),
+                id: hasAward.id,
               },
             });
-            if (updatedUser) {
+            if (deleteAward) {
               const givenAward = await prisma.award.create({
                 data: {
                   givenUserId: userId.toString(),
@@ -43,46 +88,12 @@ export const giveAward = async (c: Context) => {
                 },
               });
               if (givenAward) {
-                return c.json(
-                  { success: true, updatedPoints: player.coins - award.price },
-                  200
-                );
+                return c.json({ success: true }, 200);
               }
             }
           }
         }
-      } else {
-        const hasAward = await prisma.awardInventory.findFirst({
-          where: {
-            userId: userId.toString(),
-            awardId: award.id,
-          },
-        });
-        if (hasAward) {
-          const deleteAward = await prisma.awardInventory.delete({
-            where: {
-              userId: userId.toString(),
-              id: hasAward.id,
-            },
-          });
-          if (deleteAward) {
-            const givenAward = await prisma.award.create({
-              data: {
-                givenUserId: userId.toString(),
-                receivedUserId: recieverId.toString(),
-                reviewId: reviewId.toString(),
-                awardId: award.id.toString(),
-                time: new Date(),
-              },
-            });
-            if (givenAward) {
-              return c.json({ success: true }, 200);
-            }
-          }
-        }
       }
-      //const recentlyReviewed = getRecentlyReviewed(c); get the rest with this, highest lowest, etc
-      return c.json({ success: true }, 200);
     }
   }
   return c.json({ success: false }, 500);
