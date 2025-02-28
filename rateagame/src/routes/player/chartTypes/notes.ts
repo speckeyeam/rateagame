fb022ba15723d231adb6e24c2ad05420dacc52cf
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2025-02-28 12:23:06 -0800

    notes to self

---
+++ rateagame/src/routes/player/chartTypes/notes.ts
@@ -0,0 +1,3 @@
+//later you could probably add scripts to actually get the specific reviews in which you either recieved an award on, or gave to.
+
+//prob just need to copy the loadreviews script except have it probably look through the award isntead and include the review object
