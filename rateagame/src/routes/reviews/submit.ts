aff63fa4e97a206a228c76e7e4ce3993bb2d5e89
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2025-06-21 13:04:20 -0700

    test

--- rateagame/src/routes/reviews/submit.ts
+++ rateagame/src/routes/reviews/submit.ts
@@ -49,6 +49,7 @@ export const submit = async (c: Context) => {
           rating: recommends ? 1 : -1,
           assetId: String(gameId),
           [gamePass ? "gamePassId" : "gameId"]: String(gameId),
+          parentId,
         };
 
         const data2: any = {
