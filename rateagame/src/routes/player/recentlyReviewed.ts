fb65a470f9ffb2466cacfe1909212374c3fe41c7
speckeyeam <dicklung212@gmail.com>
2025-07-12 14:35:17 -0700

    fixed this

--- rateagame/src/routes/player/recentlyReviewed.ts
+++ rateagame/src/routes/player/recentlyReviewed.ts
@@ -7,41 +7,40 @@ const prisma = new PrismaClient();
 //gets the most trending games in the past week, games with the most reviews, could get the most trending games and games passes in the last x amount of days
 
 export const recentlyReviewed = async (c: Context) => {
-  const requestData = await c.req.json().catch(() => null); // catch in case no JSON is sent
+  const requestData = await c.req.json().catch(() => null);
 
   const { userId, date } = requestData;
 
-  if (userId && date) {
-    console.log("test");
-    const data: any = {
-      where: {
-        time: {
-          lt: new Date(date * 1000),
-        },
-        userId: userId.toString(),
-        deleted: false,
-      },
-      include: {
-        _count: {
-          select: { likes: { where: { value: true } } }, // Include the number of likes for each review
-        },
-        likes: {
-          where: { userId: userId.toString(), value: true }, // Check if the user has liked the review
-          select: { userId: true }, // Select userId to determine if a like exists
-        },
-      },
-      orderBy: {
-        time: "desc", // Orders in descending order (newest first)
-      },
-      take: 100,
-    };
-
-    const recentlyReviewed = await prisma.review.findMany(data);
-    console.log("test2");
-    if (recentlyReviewed) {
-      return c.json({ success: true, games: recentlyReviewed }, 200);
-    }
+  if (!userId || !date) {
+    return c.json({ success: false }, 500);
   }
 
-  return c.json({ success: false }, 500);
+  const recentlyReviewed = await prisma.review.findMany({
+    where: {
+      time: {
+        lt: new Date(date * 1000),
+      },
+      userId: userId.toString(),
+      deleted: false,
+    },
+    include: {
+      _count: {
+        select: { likes: { where: { value: true } } },
+      },
+      likes: {
+        where: { userId: userId.toString(), value: true },
+        select: { userId: true },
+      },
+    },
+    orderBy: {
+      time: "desc",
+    },
+    take: 100,
+  });
+
+  if (recentlyReviewed) {
+    return c.json({ success: true, games: recentlyReviewed }, 200);
+  } else {
+    return c.json({ success: false }, 500);
+  }
 };
